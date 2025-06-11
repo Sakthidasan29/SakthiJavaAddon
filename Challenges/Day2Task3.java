7ad3135b94cd429d104d9fff3c393e05ac523bb3
Sakthidasan29 <sakthi2bros@gmail.com>
2025-06-11 14:38:24 +0530

    Update and rename task3.java to Day2Task3.java

--- Challenges/Day2Task3.java
+++ Challenges/Day2Task3.java
@@ -2,7 +2,7 @@ package Challenges;
 
 //import java.util.Iterator;
 
-public class task3 {
+public class Day2Task3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
