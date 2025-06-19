91213eff0bd0fb4735f224626ed31f22fb84abb3
Sakthidasan29 <sakthi2bros@gmail.com>
2025-06-19 21:11:14 +0530

    Create Day2Task5.java

---
+++ Challenges/Day2Task5.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class arrray {
+
+
+
+	public static void main(String[] args) {
+
+
+		int[] number= {1,2,3,4,5};
+
+	System.out.println(number[3]);
+
+	System.out.println(number.length);
+
+	int count=0;
+
+		for(int i=0; i<=number.length-1; i++) {
+
+
+		count++;
+
+		System.out.println(number[i]);}
+
+		System.out.println("count is " +count);
+
+		
+		}
+
+	}
